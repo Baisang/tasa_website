1f9b73512eb9f30d6365673f1a3a98ce97956a3c
Baisang <sang.bri@gmail.com>
2015-12-20 04:48:23 -0800

    initial commit

---
+++ tasa_website/schema.sql
@@ -0,0 +1,22 @@
+drop table if exists events;
+create table events (
+  id integer primary key autoincrement,
+  title text not null,
+  time text not null,
+  location text not null,
+  link text not null,
+  image_url text not null,
+  unix_time int not null
+);
+
+drop table if exists officers;
+create table officers (
+	id integer primary key autoincrement,
+	name text not null,
+	year integer not null,
+	major text not null,
+	quote text not null,
+	description text not null,
+	image_url text not null,
+	position text not null
+);
---
+++ tasa_website/tasa_website.py
@@ -0,0 +1,143 @@
+# all the imports
+import dateutil.parser
+import os
+import requests
+import sqlite3
+import time
+
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+from datetime import datetime, date
+
+# configuration
+DATABASE = '/tmp/tasa_website.db'
+DEBUG = True
+SECRET_KEY = os.environ['TASA_SECRET']
+USERNAME = os.environ['TASA_USERNAME']
+PASSWORD = os.environ['TASA_PASSWORD']
+FACEBOOK_KEY = os.environ['TASA_FACEBOOK']
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+    g.db.row_factory = sqlite3.Row
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/')
+def show_latest_event():
+    event = query_db('select title, time, location, link, image_url from events order by unix_time desc', one=True)
+    return render_template('show_latest_event.html', event=event)
+
+@app.route('/add', methods=['POST'])
+def add_event():
+    if not session.get('logged_in'):
+        abort(401)
+    try:
+        url = request.form['link']
+        link = url
+        # Facebook event url example:
+        # https://www.facebook.com/events/1201801539835081/
+        # we need to get the last part, the id. so let's find the index of the 2nd to last '/'
+        # split the string and go from there.
+        index = url.rfind('/', 0, url.rfind('/'))
+        fb_event_id = url[index : len(url) - 1]
+
+        # need to submit GET req to something like this:
+        # https://graph.facebook.com/v2.5/1201801539835081?access_token=token
+        fb_api_base = 'https://graph.facebook.com/v2.5/'
+        fb_api_base += fb_event_id
+        payload = {"access_token": FACEBOOK_KEY}
+        res = requests.get(fb_api_base, params=payload).json()
+        title = res['name']
+        location = res['place']['name']
+        time_str = res['start_time']
+        date_time = dateutil.parser.parse(time_str)
+        time_str = date_time.strftime("%A %B %d %I:%M%p")
+        unix_time = int(time.mktime(date_time.timetuple()) + date_time.microsecond/1000000.0)
+
+        # another GET to get the cover photo..?
+        payload = {"access_token": FACEBOOK_KEY, "fields": "cover"}
+        res = requests.get(fb_api_base, params=payload).json()
+        image_url = res['cover']['source']
+
+        query = 'insert into events (title, time, location, link, image_url, unix_time)'\
+                'values (?, ?, ?, ?, ?, ?)'
+        g.db.execute(query, [title, time_str, location, link, image_url, unix_time])
+        g.db.commit()
+        flash('New event was successfully posted')
+        return redirect(url_for('admin_panel'))
+    except e:
+        print str(e)
+        return redirect(url_for('admin_panel'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME:
+            error = 'Invalid username'
+        elif request.form['password'] != PASSWORD:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('admin_panel'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_latest_event'))
+
+@app.route('/admin', methods=['GET'])
+def admin_panel():
+    if not session.get('logged_in'):
+        abort(401)
+    events = query_db('select title from events order by unix_time desc')
+    return render_template('admin.html', events=events)
+
+@app.route('/events', methods=['GET'])
+def event_list():
+    query = 'select title, time, location, link, image_url, unix_time '\
+            'from events order by unix_time desc limit 24'
+    events = query_db(query)
+    upcoming = []
+    recent = []
+    current_time = int(time.time())
+    for event in events:
+        if event['unix_time'] > current_time:
+            # key thing here to remember was append vs extend, 
+            # since event is a sqlite row obj and iterable
+            upcoming.append(event)
+        else:
+            recent.append(event)
+    return render_template('events.html', recent=recent, upcoming=upcoming)
+
+if __name__ == '__main__':
+    app.run()
